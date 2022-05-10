b093c156cd27d02282e3b66dae5b1e6893c51674
Afra Sabei <92374517+Afra-S@users.noreply.github.com>
2022-05-10 17:06:26 +0200

    Update sep.sh

--- sep.sh
+++ sep.sh
@@ -1,5 +1,4 @@
 
-#for complex in 'ls /disk4T/prot_RNA/complex-contact';
 for complex in  1JBS 1MFQ 1RKJ 2VPL 3IEV #1E7K
 do
    (cd "$complex" && gmx trjconv -f md_500.xtc -s start.pdb -skip 10 -sep -o sep_snap_.pdb<<EOF
